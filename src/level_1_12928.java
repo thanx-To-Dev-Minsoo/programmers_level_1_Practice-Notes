0970c0d6c2eea67339c1031dc9426c8be75cfc9d
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-11 14:26:15 +0900

    2024-07-11

--- src/level_1_12928.java
+++ src/level_1_12928.java
@@ -1,12 +1,27 @@
 //약수의 합
 //정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution 을 완성해주세요.
 
+//public class level_1_12928 {
+//    public int solution (int n) {
+//        int answer = 0;
+//
+//        for (int i = 1; i <= n; i++) {
+//            if (n % i == 0) {
+//                answer += i;
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
+
+
 public class level_1_12928 {
     public int solution (int n) {
         int answer = 0;
 
         for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
+            if (n % i == 0){
                 answer += i;
             }
         }
