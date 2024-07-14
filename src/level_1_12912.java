bba198a628b0bfd8bc95fdd7da290248f94e306c
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-14 12:22:43 +0900

    2024-07-14

--- src/level_1_12912.java
+++ src/level_1_12912.java
@@ -2,6 +2,29 @@
 //두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution 을 완성하세요.
 //예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
 
+import java.util.Scanner;
+
+public class level_1_12912 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+
+        for (int i = start; i <= end; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
+
 //public class level_1_12912 {
 //    public long solution (int a, int b) {
 //        long sum = 0;
@@ -20,17 +43,17 @@
 //}
 
 
-public class level_1_12912 {
-    public long solution(int a, int b) {
-        long sum = 0;
-
-        int start = Math.min(a, b);
-        int end = Math.max(a, b);
-
-        for (int i = start; i <= end; i++) {
-            sum += i;
-        }
-
-        return sum;
-    }
-}
+//public class level_1_12912 {
+//    public long solution(int a, int b) {
+//        long sum = 0;
+//
+//        int start = Math.min(a, b);
+//        int end = Math.max(a, b);
+//
+//        for (int i = start; i <= end; i++) {
+//            sum += i;
+//        }
+//
+//        return sum;
+//    }
+//}
