59d016b5bc2894bfe3a972f616a47687690a3563
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-28 10:10:00 +0900

    2024-07-16

--- src/level_1_12912.java
+++ src/level_1_12912.java
@@ -24,36 +24,3 @@ public class level_1_12912 {
         sc.close();
     }
 }
-
-//public class level_1_12912 {
-//    public long solution (int a, int b) {
-//        long sum = 0;
-//
-//        //a와 b의 대소 관계를 파악하여 작은 값을 start 에, 큰 값을 end 에 저장합니다.
-//        int start = Math.min(a, b);
-//        int end = Math.max(a, b);
-//
-//        //start 부터 end 까지 모든 정수를 더합니다.
-//        for (int i = start; i <= end; i++) {
-//            sum += i;
-//        }
-//
-//        return sum;
-//    }
-//}
-
-
-//public class level_1_12912 {
-//    public long solution(int a, int b) {
-//        long sum = 0;
-//
-//        int start = Math.min(a, b);
-//        int end = Math.max(a, b);
-//
-//        for (int i = start; i <= end; i++) {
-//            sum += i;
-//        }
-//
-//        return sum;
-//    }
-//}
